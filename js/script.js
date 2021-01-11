685f3ad1c2f708a7d11ba0af1606e7a1102eac08
Daniel Kim <developer.danielkim@gmail.com>
2021-01-11 06:51:17 -0800

    1/11/2021: multiple UI updates

--- js/script.js
+++ js/script.js
@@ -2,28 +2,32 @@ var slideIndex = 0;
 showSlides(slideIndex);
 
 function currentSlide(index) {
-  showSlides(slideIndex = index);
+  showSlides((slideIndex = index));
 }
 
-function muteIndicators(index){
-    var indicators = document.getElementsByClassName("page-indicator");
-    for (i = 0; i < indicators.length; i++) {
-        if (i != index){
-            indicators[i].style.opacity = '.1'
-        }else{
-            indicators[i].style.opacity = '1'
-        }
+function muteIndicators(index) {
+  var indicators = document.getElementsByClassName("page-indicator");
+  for (i = 0; i < indicators.length; i++) {
+    if (i != index) {
+      indicators[i].style.opacity = ".1";
+    } else {
+      indicators[i].style.opacity = "1";
     }
+  }
 }
 
 function showSlides(index) {
   var i;
   var slides = document.getElementsByClassName("slide");
-  muteIndicators(index)
-  if (index >= slides.length) {slideIndex = 0}
-  if (index < 0) {slideIndex = slides.length}
+  muteIndicators(index);
+  if (index >= slides.length) {
+    slideIndex = 0;
+  }
+  if (index < 0) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   slides[slideIndex].style.display = "block";
 }
